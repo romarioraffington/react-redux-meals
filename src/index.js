7454a952f469d19f7c15fff0e0eb846772adcd81
Romario Raffington <romarioraff@gmail.com>
2017-08-05 22:54:41 -0500

    feat: Added redux to the app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 // External Dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 
 // Our Dependencies
 import './index.css';
 import App from './components/App';
+import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
 <App />,
