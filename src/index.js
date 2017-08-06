1183f464aefed26149a4e62b26d88281bc57f49c
Romario Raffington <romarioraff@gmail.com>
2017-08-05 21:30:09 -0500

    refactor:  Added comments for readability

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+// External Dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Our Dependencies
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<App />,
+document.getElementById('root'));
 registerServiceWorker();
