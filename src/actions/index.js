3756245749cee0f1b50f64e9765b7d0d0159e336
Romario Raffington <romarioraff@gmail.com>
2017-08-05 21:28:16 -0500

    feat: Added the constants, actions and reducer to add and remove a recipe

---
+++ src/actions/index.js
@@ -0,0 +1,21 @@
+import {
+  ADD_RECIPE,
+  REMOVE_FROM_CALENDAR
+} from '../constants';
+
+export function addRecipe ({ day, recipe, meal }) {
+  return {
+    type: ADD_RECIPE,
+    recipe,
+    day,
+    meal,
+  }
+}
+
+export function removeFromCalendar ({ day, meal }) {
+  return {
+    type: REMOVE_FROM_CALENDAR,
+    day,
+    meal,
+  }
+}
