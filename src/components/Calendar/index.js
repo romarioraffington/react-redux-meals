a30b28e5c7771b8ff58e04ba653f035265304a12
Romario Raffington <romarioraff@gmail.com>
2017-08-08 12:28:01 -0500

    feat: Added state and modal functions

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import CalendarIcon from 'react-icons/lib/fa/calendar-plus-o';
 
+// Our Components
+import FoodModal from './FoodModal';
+
 // Redux
 function mapStateToProps({ calendar }, { mealOrders }) {
   const days = [
@@ -32,39 +35,75 @@ function mapStateToProps({ calendar }, { mealOrders }) {
 
 // Component
 class Calendar extends Component {
+
+  state = {
+    meal: null,
+    day: null,
+    food: null,
+    loadingFood: false,
+    foodModalOpen: false,
+    ingredientsModalOpen: false,
+  }
+
+  openFoodModal = ({ meal, day }) => {
+    this.setState(() => ({
+      foodModalOpen: true,
+      meal,
+      day,
+    }))
+  }
+
+  closeFoodModal = () => {
+    this.setState(() => ({
+      foodModalOpen: false,
+      meal: null,
+      day: null,
+      food: null,
+    }))
+  }
+
+
   render() {
+    const { loadingFood, meal, day } = this.state;
     const { mealOrders, calendar } = this.props;
 
     return (
-      <div className="calendar">
-        <div className="days">
-          { calendar.map(({ day }) => 
-            <h3 key={day} className="subheader">
-              {day}
-            </h3>
-          )}
+      <div>
+        <div className="calendar">
+          <div className="days">
+            { calendar.map(({ day }) => 
+              <h3 key={day} className="subheader">
+                {day}
+              </h3>
+            )}
+          </div>
+          <div className="icon-grid">
+            { calendar.map(({ day, meals }) => (
+              <ul key={day}>
+                { mealOrders.map(meal => (
+                  <li key={meal} className="meal">
+                    { meals[meal] 
+                        ? <div className="food-item">
+                            <img src={meals[meal].image} alt={meals[meal].label}/>
+                          </div>
+                        : <button className="icon-btn" onClick={() => this.openFoodModal({ meal, day })}>
+                            <CalendarIcon size={30} /> 
+                          </button>        
+                    }
+                  </li>
+                ))}
+              </ul>
+            ))}
+          </div>
         </div>
-        <div className="icon-grid">
-          { calendar.map(({ day, meals }) => (
-            <ul key={day}>
-              { mealOrders.map(meal => (
-                <li key={meal} className="meal">
-                  { meals[meal] 
-                      ? <div className="food-item">
-                          <img 
-                            src={meals[meal].image} 
-                            alt={meals[meal].label}
-                            />
-                        </div>
-                      : <button className="icon-btn">
-                          <CalendarIcon size={30} /> 
-                        </button>        
-                  }
-                </li>
-              ))}
-            </ul>
-          ))}
-        </div>
+        
+        <FoodModal 
+          meal={meal} 
+          day={day}
+          loadingFood={loadingFood}
+          foodModalOpen={this.foodModalOpen}
+          closeFoodModal={this.closeFoodModal}
+        />
       </div>
     )
   }
